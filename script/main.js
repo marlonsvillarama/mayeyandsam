dbf3797d90ce903f7773180278ab3e4167b5ccb7
Marlon Villarama <marlonsvillarama@gmail.com>
2026-02-10 11:55:31 +1300

    20260210

--- script/main.js
+++ script/main.js
@@ -7,3 +7,14 @@ document.getElementById('avail').addEventListener('click', (e) => {
     const dlg = document.querySelector('dialog');
     dlg.showModal();
 });
+
+document.getElementById('cancel_avail').addEventListener('click', (e) => {
+    // let parallax = document.querySelector('.parallax');
+    // console.log('parallax', parallax);
+    // parallax.style.backgroundImage = 'url("/images/marielle_and_sam_06.png")';
+
+    console.log('avail click');
+    const dlg = document.querySelector('dialog');
+    dlg.close();
+});
+
