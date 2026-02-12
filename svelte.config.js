442cf0278bc9a10f8c56b74e9439d4ba2a3616e2
Marlon Villarama <marlonsvillarama@gmail.com>
2026-02-13 08:58:09 +1300

    20260213

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
@@ -5,4 +6,16 @@ export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+      precompress: false,
+      strict: true
+    }),
+    prerender: {
+      default: true
+    }
+  }
 }
