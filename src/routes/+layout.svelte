1c1febb126b965a5a9bc4863bba2b8252dec45a9
Marlon Villarama <marlonsvillarama@gmail.com>
2026-02-17 09:10:58 +1300

    20260217

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/heart.svg';
 	import '../style/main.css';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
+	<title>Sam & Marielle - SAVE THE DATE</title>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
